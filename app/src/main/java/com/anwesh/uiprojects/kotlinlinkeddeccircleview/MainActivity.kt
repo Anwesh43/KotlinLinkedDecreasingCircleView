48a7abd147392a1ba95c264fab556b568438427d
Anwesh43 <anweshthecool0@gmail.com>
2018-07-09 11:20:12 +0530

    adding OnCompletionListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkeddeccircleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkeddeccircleview/MainActivity.kt
@@ -3,14 +3,18 @@ package com.anwesh.uiprojects.kotlinlinkeddeccircleview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linkeddeccircleview.LinkedDecCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedDecCircleView.create(this)
+        val view = LinkedDecCircleView.create(this)
         fullScreen()
+        view.setOnCompletionListener {
+            Toast.makeText(this, "animation number ${it + 1} completed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
